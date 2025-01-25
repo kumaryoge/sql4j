e822a684122b3bf1fdcac9e3ba3dc8672be1ca6b
Yogendra Kumar <11089351+kumaryoge@users.noreply.github.com>
2025-01-25 11:04:23 -0800

    Add license headers in java files

--- src/main/java/org/sql4j/sql/query/Table.java
+++ src/main/java/org/sql4j/sql/query/Table.java
@@ -1,5 +1,25 @@
 package org.sql4j.sql.query;
 
+/*-
+ * ==============================LICENSE_START==============================
+ * io.github.kumaryoge:sql4j
+ * --
+ * Copyright (C) 2025 io.github.kumaryoge
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============================LICENSE_END===============================
+ */
+
 import lombok.Getter;
 import lombok.NonNull;
 
